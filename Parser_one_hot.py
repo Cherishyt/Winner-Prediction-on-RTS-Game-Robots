7d1815b6d0f480daeac8816906f44a2e3aa616a3
Cherishyt <37820678+Cherishyt@users.noreply.github.com>
2019-12-04 10:08:31 +0800

    Update Parser_one_hot.py

--- Parser_one_hot.py
+++ Parser_one_hot.py
@@ -2,12 +2,12 @@ from xml.etree import ElementTree as ET
 import numpy as np
 import os
 
-
-#数据路径
+#the path of reading dataset
 rootpath=r'F:\yutian\Datasets\InterceptedDatasets\sample_part_20\samplepart1'
+#the path of saving encoding npy files
 targetpath=r'F:\yutian\Datasets\InterceptedDatasets\sample_part_20_encoding\samplepart1'
 ONEHOT_SIZE=38
-ALL_UNIT_TYPES = ['RESOURCE','BASE', 'BARRACKS', 'WORKER', 'LIGHT', 'RANGED','HEAVY']#七位来编码单位类型
+ALL_UNIT_TYPES = ['RESOURCE','BASE', 'BARRACKS', 'WORKER', 'LIGHT', 'RANGED','HEAVY']
 ALL_RESOURCES=[1,2,3,4,5]
 ALL_HP=[1,2,3,4]
 ALL_ACTION_TYPES=[0,1,2,3,4,5]
@@ -15,9 +15,9 @@ np.set_printoptions(threshold=np.inf)
 
 if __name__ == '__main__':
     def encoding(width,height,units_node,actions_node,p_resources):
-        map = np.ndarray((int(width), int(height),ONEHOT_SIZE))#创建三维数组（w,h,38）
+        map = np.ndarray((int(width), int(height),ONEHOT_SIZE))#create（w,h,38）
         map.fill(0)
-        #编码units数据
+        #encoding units data
         unit_len=len(units_node)
         for unit_node in units_node:
             x=int(unit_node.get('x'))
@@ -42,16 +42,15 @@ if __name__ == '__main__':
                 #a=ALL_RESOURCES.index(int(p_resources[1]))
 
             #resources,9-15
-            #单独编码Base的resources值
             #if type=='Base':
             #    map[y,x,9+a]=1
             #else:
             resources=int(unit_node.get('resources'))
-            if resources==0:#不用编码
+            if resources==0:
                 pass
             elif resources<6:
                 j=ALL_RESOURCES.index(resources)
-                map[y,x,9+j]=1#编码位为9,10,11,12,13
+                map[y,x,9+j]=1
             elif resources<10:
                 map[y,x,14]=1
             elif resources>=10:
@@ -59,20 +58,20 @@ if __name__ == '__main__':
 
             #hitpoints,16-20
             hitpoints=int(unit_node.get('hitpoints'))
-            if hitpoints==0:#不用编码
+            if hitpoints==0:
                 pass
             elif hitpoints<5:
                 k=ALL_HP.index(hitpoints)
-                map[y,x,16+k]=1#编码位为16,17,18,19
+                map[y,x,16+k]=1
             elif hitpoints>=5:
                 map[y,x,20]=1
 
-        #编码action数据
+        #encoding action data
         if actions_node:
             for action_node in actions_node:
                 unitid=action_node.get('unitID')
 
-                #查找有action的unit
+                #look for action's unit by ID
                 for unit_node in units_node:
                     id=unit_node.get('ID')
                     if unitid==id:
@@ -93,18 +92,18 @@ if __name__ == '__main__':
                 else:
                     #parameter
                     a_parameter=int(action_node.find('UnitAction').get('parameter'))
-                    if a_parameter==0:#上
+                    if a_parameter==0:#up
                         b_y=b_y-1
-                    elif a_parameter==1:#右
+                    elif a_parameter==1:#right
                         b_x=b_x+1
-                    elif a_parameter==2:#下
+                    elif a_parameter==2:#down
                         b_y=b_y+1
-                    elif a_parameter==3:#左
+                    elif a_parameter==3:#left
                         b_x=b_x-1
                 l=l-1
-                if l>-1:#被动承受位，27-31
+                if l>-1:#passive action type，27-31
                     map[b_y,b_x,27+l]=1
-                if a_type==4:#生成单元类型位,32-37
+                if a_type==4:#produce unitType,32-37
                     #unitType
                     b_type=action_node.find('UnitAction').get('unitType')
                     m=ALL_UNIT_TYPES.index(b_type.upper())-1
@@ -123,28 +122,24 @@ if __name__ == '__main__':
     if not os.path.exists(txt_path):
         os.mkdir(txt_path)
     '''
-    for file in os.listdir(rootpath):#遍历所有xml文件
-        #获取文件名称
+    for file in os.listdir(rootpath):
+        #get file name
         filename=os.path.splitext(file)[0]
         print(filename)
 
-        #解析
         tree=ET.parse(os.path.join(rootpath, file))
-        #获取根节点
         root=tree.getroot()
 
-        #获取winner
+        #get label
         winner_node=root.find('winner')
         winner=int(winner_node.get('winner'))
         #print("winner:")
         #print(winner)
-        #平局情况
+        #draw
         if winner==-1:
-            #第一种，平局为winner0
             #winner=0
-            #第二种，平局为winner1
             #winner=1
-            #第三种，随机
+            #random
             #'''
             r=np.random.randint(0,2)
             if r==0:
@@ -154,16 +149,15 @@ if __name__ == '__main__':
             #'''
         #print(winner)
 
-        #获取待编码的数据
         traceentry_node=root.find('rts.TraceEntry')
         #time
         time=traceentry_node.get('time')
         #print("time="+time)
         pgs_node=traceentry_node.find('rts.PhysicalGameState')
-        #width和height
+        #width and height
         width=pgs_node.get('width')
         height=pgs_node.get('height')
-        #player0和player1的resources值
+        #player0's and player1's resources
         p_resources=[0,0]
         players_node=pgs_node.find('players')
         i=0
@@ -171,22 +165,21 @@ if __name__ == '__main__':
             p_resources[i]=player_node.get('resources')
             #print(p_resources[i])
             i+=1
-        #<units>下的数据
+        #<units
         units_node=pgs_node.find('units')
-        #<actions>下的数据
+        #<actions>
         actions_node=traceentry_node.find('actions')
-        #编码
+        #encoding
         x=encoding(width,height,units_node,actions_node,p_resources)
         #print(x)
 
-        #写入npy文件
-
+        #write into npy file
         newfilename=filename+'-'+str(winner)+'.npy'
         newfilepath=os.path.join(npy_path, newfilename)
         print(newfilepath)
         np.save(newfilepath,x)
 
-        #写入TXT文件
+        #write into txt file
         '''
         newtxtname=filename+'-'+str(winner)+'.txt'
         newtxtpath=os.path.join(txt_path,newtxtname)
@@ -195,15 +188,3 @@ if __name__ == '__main__':
         newtxt=open(newtxtpath,'w')
         print(test,file=newtxt)
         '''
-
-
-
-
-
-
-
-
-
-
-
-
