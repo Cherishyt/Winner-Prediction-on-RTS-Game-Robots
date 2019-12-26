2fc9d0ec0aa489022c65f550bceb548dd27020be
Cherishyt <37820678+Cherishyt@users.noreply.github.com>
2019-12-26 11:09:57 +0800

    Update train_BayesByBackprop.py

--- BNN model/train_BayesByBackprop.py
+++ BNN model/train_BayesByBackprop.py
@@ -8,11 +8,10 @@ from src.Bayes_By_Backprop.model import *
 #from src.Bayes_By_Backprop_Local_Reparametrization.model import *
 from sklearn.model_selection import train_test_split
 import os
-
+#the path of npy files
 rootpath=r'F:\yutian\Datasets\datasets_nonMCTS\randomsample_encoding_ont_hot\npy'
 models_dir = "BBP_model"
 results_dir = "BBP_results"
-#创建文件夹
 mkdir(models_dir)
 mkdir(results_dir)
 # train config
@@ -23,31 +22,27 @@ nb_epochs = 100
 lr=1e-4
 
 # load data
-#读取所有数据
 X,Y=[],[]
-for file in os.listdir(rootpath):#遍历所有npy文件
+for file in os.listdir(rootpath):
     filename=os.path.splitext(file)[0]
-    #获取winner作为label
+    #get labels
     winner=int(filename[-1:])
-    #Y.append(winner)
-    #print(winner)
-    #'''
+    #one-hot
     if winner==0:
         Y.append([1,0])
     elif winner==1:
         Y.append([0,1])
     else:
         raise ValueError("Invalid winner value!")
-    #'''
-    #三维数组，作为输入数据
+
     x=np.load(os.path.join(rootpath,file))
     X.append(x)
 
-X = np.array(X)  # 变成数组形式,应该是四维(,8,8,38)
-Y = np.array(Y)  #变成数组形式，应该是二维(,2)
+X = np.array(X)  #(,8,8,38)
+Y = np.array(Y)  #(,2)
 print(X.shape)
 print(Y.shape)
-# 50%训练集，25%验证集，25%测试集
+#50% train sets，25% val sets，25% test set
 X_train, X_val_test, Y_train, Y_val_test = train_test_split(X, Y, test_size=1 / 2)
 X_test, X_val, Y_test, Y_val = train_test_split(X_val_test, Y_val_test, test_size=1 / 2)
 print(X_train.shape)
@@ -60,13 +55,7 @@ print(Y_test.shape)
 NTrainPoints=X_train.shape[0]
 print(NTrainPoints)
 use_cuda = torch.cuda.is_available()
-'''
-trainset=Data.TensorDataset(X_train,Y_train)
-valset=Data.TensorDataset(X_val,Y_val)
-print(trainset.shape)
-print(valset.shape)
-'''
-# 定义一个函数，按批次取数据
+
 def minibatches(inputs=None, targets=None, batch_size=None, shuffle=False):
     assert len(inputs) == len(targets)
     if shuffle:
@@ -78,31 +67,14 @@ def minibatches(inputs=None, targets=None, batch_size=None, shuffle=False):
         else:
             excerpt = slice(start_idx, start_idx + batch_size)
         yield inputs[excerpt], targets[excerpt]
-#每次取batch_size=64条数据
-#shuffle=True时每次重新整理数据
-'''
-if use_cuda:
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, pin_memory=True,
-                                              num_workers=3)
-    valloader = torch.utils.data.DataLoader(valset, batch_size=batch_size, shuffle=False, pin_memory=True,
-                                            num_workers=3)
 
-else:
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, pin_memory=False,
-                                              num_workers=3)
-    valloader = torch.utils.data.DataLoader(valset, batch_size=batch_size, shuffle=False, pin_memory=False,
-                                            num_workers=3)
-'''
 
-#建立网络模型
+#model
 net = BBP_Bayes_Net(lr=lr, channels_in=38, side_in=8, cuda=use_cuda, classes=2, batch_size=batch_size,
                     Nbatches=(NTrainPoints/ batch_size), nhid=1200, prior_instance=laplace_prior(mu=0, b=0.1))
                     #prior_instance=isotropic_gauss_prior(mu=0, sigma=0.1))
 
 
-
-#训练和测试
-#np.zeros返回一个给定形状和类型的用0填充的数组；
 kl_cost_train = np.zeros(nb_epochs)
 pred_cost_train = np.zeros(nb_epochs)
 err_train = np.zeros(nb_epochs)
@@ -111,24 +83,23 @@ acc_train=np.zeros(nb_epochs)
 cost_dev = np.zeros(nb_epochs)
 err_dev = np.zeros(nb_epochs)
 acc_dev = np.zeros(nb_epochs)
-best_err = np.inf#指无穷大？
+best_err = np.inf
 
 nb_its_dev = 1
 
-tic0 = time.time()#返回当前时间的时间戳
-#循环40次，i=0,...,39
+tic0 = time.time()
 for i in range(0, nb_epochs):
     # We draw more samples on the first epoch in order to ensure convergence
     if i == 0:
         ELBO_samples = 10
     else:
-        ELBO_samples = 3#默认3
+        ELBO_samples = 3
 
     net.set_mode_train(True)
     tic = time.time()
     nb_samples = 0
 
-    #训练
+    #train
     for x, y in minibatches(X_train, Y_train, batch_size, shuffle=True):
         cost_dkl, cost_pred, err,acc = net.fit(x, y, samples=ELBO_samples)
 
@@ -150,7 +121,7 @@ for i in range(0, nb_epochs):
     i, nb_epochs, kl_cost_train[i], pred_cost_train[i], err_train[i],acc_train[i]))
 
     # ---- dev
-	#验证
+    #val
     if i % nb_its_dev == 0:
         net.set_mode_train(False)
         nb_samples = 0
@@ -193,15 +164,6 @@ print('  err_dev: %f' % (err_dev_min))
 print('  nb_parameters: %d (%s)' % (nb_parameters, humansize(nb_parameters)))
 print('  time_per_it: %fs\n' % (runtime_per_it))
 
-## Save results for plots
-# np.save('results/test_predictions.npy', test_predictions)
-'''
-np.save(results_dir + '/KL_cost_train.npy', kl_cost_train)
-np.save(results_dir + '/pred_cost_train.npy', pred_cost_train)
-np.save(results_dir + '/cost_dev.npy', cost_dev)
-np.save(results_dir + '/err_train.npy', err_train)
-np.save(results_dir + '/err_dev.npy', err_dev)
-'''
 
 #test
 cost_test=0
@@ -209,7 +171,6 @@ err_test=0
 acc_test=0
 nb_samples=0
 net.load(models_dir + '/theta_best.dat')
-#for x, y in minibatches(X_test, Y_test, test_batch_size, shuffle=True):
 cost, err, probs, acc,y_score = net.eval(X_test, Y_test)  # This takes the expected weights to save time, not proper inference
 
 cost_test += cost
@@ -223,8 +184,7 @@ acc_test /= nb_samples
 
 print('  acc_test: %f' % (acc_test))
 
-#计算auc
-#print(best_out)
+#AUC
 fpr = dict()
 tpr = dict()
 roc_auc = dict()
