fa5e69c2f0a192462af0f0b95a066937449bef22
Cherishyt <37820678+Cherishyt@users.noreply.github.com>
2020-06-28 20:46:26 +0800

    Update prepare_data.py

--- CNN model/prepare_data.py
+++ CNN model/prepare_data.py
@@ -4,31 +4,42 @@ import os
 
 #get train sets,val sets and test sets
 def getalldata(filepath):
-    X, Y = [], []
+    T, W = [], []
     for file in os.listdir(filepath):
         filename = os.path.splitext(file)[0]
         #get labels
         winner = int(filename[-1:])
         #one-hot
         if winner == 0:
-            Y.append([1, 0])
+            W.append([1, 0])
         elif winner == 1:
-            Y.append([0, 1])
+            W.append([0, 1])
         else:
             raise ValueError("Invalid winner value!")
-
-        x = np.load(os.path.join(filepath, file))
-        X.append(x)
-    X = np.array(X)  #(,8,8,39)
-    Y = np.array(Y)  #(,2)
+        t = np.load(os.path.join(filepath, file))
+        T.append(t)
+    T = np.array(T)  #(,8,8,39)
+    W = np.array(W)  #(,2)
 
     #50% train sets，25% val sets，25% test set
-    X_train, X_val_test, Y_train, Y_val_test = train_test_split(X, Y, test_size=1 / 2)
-    X_test,X_val,Y_test,Y_val=train_test_split(X_val_test,Y_val_test,test_size=1/2)
-    print(X_train.shape)
-    print(Y_train.shape)
-    print(X_val.shape)
-    print(Y_val.shape)
-    print(X_test.shape)
-    print(Y_test.shape)
-    return X_train, Y_train, X_val, Y_val,X_test,Y_test
+    T_train, T_val_test,W_train, W_val_test = train_test_split(T, W, test_size=1 / 2)
+    T_test,T_val,W_test,W_val=train_test_split(T_val_test,W_val_test,test_size=1/2)
+    print(T_train.shape)
+    print(W_train.shape)
+    print(T_val.shape)
+    print(W_val.shape)
+    print(T_test.shape)
+    print(W_test.shape)
+    return T_train, W_train, T_val, W_val,T_test,W_test
+
+def minibatches(inputs=None, targets=None, batch_size=None, shuffle=False):
+    assert len(inputs) == len(targets)
+    if shuffle:
+        indices = np.arange(len(inputs))
+        np.random.shuffle(indices)
+    for start_idx in range(0, len(inputs) - batch_size + 1, batch_size):
+        if shuffle:
+            excerpt = indices[start_idx:start_idx + batch_size]
+        else:
+            excerpt = slice(start_idx, start_idx + batch_size)
+        yield inputs[excerpt], targets[excerpt]
